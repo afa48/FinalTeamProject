402648e735e43053bae11c1e869d6469d1779833
Azra <azraawan@yahoo.com>
2021-05-02 12:42:55 -0400

    Task: edit code in app.py to import chart

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, Response, redirect
 from flask import render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
+from chart.js import Chart
 
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
