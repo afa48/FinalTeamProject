a78bfbfdb75d697af92a9b1239648fe99eed4460
Azra <azraawan@yahoo.com>
2021-05-02 14:19:40 -0400

    Task: Add code in app.py for chart

--- app/app.py
+++ app/app.py
@@ -16,10 +16,15 @@ app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'crashesData'
 mysql.init_app(app)
 
+@app.route("/")
+def chart():
+    labels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    Values = [13664, 17279, 17337, 17394, 17954, 19147, 15714]
+    return render_template('chart.html', values=values, labels=labels)
 
 @app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Number of Car Crashes in Catalonia from 2000 to 2011'}
+    user = {'username': '# of Car Crashes in Catalonia, Spain: 2000 - 2011'}
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM crash_catalonia')
     result = cursor.fetchall()
