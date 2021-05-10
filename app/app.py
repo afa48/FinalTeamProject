382489a5fcea62d2ac1af22e189dd29e75a97526
Azra <azraawan@yahoo.com>
2021-05-10 11:16:44 -0400

    Task: Updated app.py/chart.html so that hardcoded graph works; Added register.html page for email registration process

--- app/app.py
+++ app/app.py
@@ -130,11 +130,19 @@ def api_delete(crash_id) -> str:
 
 @app.route('/view/chart', methods=['GET'])
 def display_chart():
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM crash_catalonia')
-    result = cursor.fetchall()
-    json_result = json.dumps(result);
-    return render_template('chart.html', title='Chart of Crashes', dataSet=result)
+    return render_template('chart.html', title='Chart of Crashes')
+
+# @app.route('/edit/<int:chart_id>', methods=['GET'])
+# def chart_edit_get(chart_id):
+#     print("Labels", )
+
+# @app.route('/view/chart', methods=['GET'])
+# def display_chart():
+#     cursor = mysql.get_db().cursor()
+#     cursor.execute('SELECT * FROM crash_catalonia')
+#     result = cursor.fetchall()
+#     json_result = json.dumps(result);
+#     return render_template('chart.html', title='Chart of Crashes', dataSet=result)
 
 
 if __name__ == '__main__':
